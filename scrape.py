2cbf07f94e1c68577e82a71b632d1aebed32b8f0
Issa Rice <riceissa@gmail.com>
2018-02-12 18:22:55 -0800

    do more stuff

--- scrape.py
+++ scrape.py
@@ -2,19 +2,48 @@
 
 import pdb
 
+import requests
 from bs4 import BeautifulSoup
 
+CAUSE_URLS = ["https://www.google.org/our-work/education/",
+              "https://www.google.org/our-work/economic-opportunity/",
+              "https://www.google.org/our-work/inclusion/",
+              "https://www.google.org/our-work/crisis-response/"]
+
+
 def main():
-    with open("economic-opportunity.html", "r") as f:
+    with open("education.html", "r") as f:
         soup = BeautifulSoup(f, "lxml")
         grants = []
-        for grantee in soup.find_all("section", {"class": "single-accordion"}):
+
+        grant_urls = []
+        prefix = "education"
+        for link in soup.find_all("a"):
+            if str(link.get("href")).startswith("/our-work/" + prefix + "/"):
+                grant_url = "https://www.google.org" + link.get("href")
+                if grant_url not in grant_urls:
+                    grant_urls.append(grant_url)
+
+        for grant_url in grant_urls:
+            grants.append(grant_info(grant_url))
+
+        for grantee in soup.find_all("section", {"class": "single-accordion--static"}):
             grantee_dict = {"grantee": grantee.find("h3").text.strip(),
-                            "description": grantee.find("p", {"class": "single-accordion__description"}).text.strip()}
+                            "description": grantee.find("p",
+                                {"class": "single-accordion__description"})
+                                .text.strip()}
             for key, val in zip(grantee.find_all("dt"), grantee.find_all("dd")):
                 grantee_dict[key.text] = val.text
             grants.append(grantee_dict)
         pdb.set_trace()
+
+
+def grant_info(grant_url):
+    soup = BeautifulSoup(requests.get(grant_url).content, "lxml")
+    grantee_dict = {"grantee": soup.find("h1").text.strip()}
+    for key, val in zip(grantee.find_all("dt"), grantee.find_all("dd")):
+        grantee_dict[key.text] = val.text
+    return grantee_dict
 
 
 if __name__ == "__main__":
